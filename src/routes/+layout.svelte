856fad76eb99fd9bd7f201ce6c5051e9ee55889b
Kevin Tindall <kevinkjt2000@gmail.com>
2023-05-04 14:53:50 -0500

    Use +layout feature and add github icon

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<main>
+	<slot />
+</main>
+
+<style>
+	@import 'lineicons/web-font/lineicons.css';
+
+	:root {
+		--box-background: white;
+		--box-border: black;
+		--box-shadow: gray;
+	}
+
+	main {
+		display: grid;
+		grid-column-gap: 1rem;
+		grid-auto-rows: min-content;
+		grid-template-columns: 1fr 1fr;
+		grid-template-areas: 'grid-left' 'grid-right';
+		max-width: 1200px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,49 +2,32 @@
 	import Box from '$lib/Box.svelte';
 </script>
 
-<main>
-	<div class="grid-left">
-		<Box title="Hi, I'm Kevin!">
-			I stare at lights and press buttons, and I'm good at it! Of course, I discuss necessary
-			requirements beforehand so that I can press buttons in a way that solves what is asked for.
-		</Box>
+<div class="grid-left">
+	<Box title="Hi, I'm Kevin!">
+		I stare at lights and press buttons, and I'm good at it! Of course, I discuss necessary
+		requirements beforehand so that I can press buttons in a way that solves what is asked for.
+	</Box>
 
-		<Box title="Stuff I do for fun">
-			When I'm not working, I like to solve <a
-				href="https://en.wikipedia.org/wiki/Combination_puzzle">combination puzzles</a
-			>, play video games, volunteer to teach beginning programmers, and tinker with code.
-		</Box>
-	</div>
+	<Box title="Stuff I do for fun">
+		When I'm not working, I like to solve <a href="https://en.wikipedia.org/wiki/Combination_puzzle"
+			>combination puzzles</a
+		>, play video games, volunteer to teach beginning programmers, and tinker with code.
+	</Box>
+</div>
 
-	<div class="grid-right">
-		<Box title="Tech I often use">C++ Python Svelte</Box>
+<div class="grid-right">
+	<Box title="Tech I often use">C++ Python Svelte</Box>
 
-		<Box title="Links">
-			<ul>
-				<li><a href="https://discord.gg/J5hBe8F">C++ Help Discord</a></li>
-				<li><a href="https://github.com/kevinkjt2000">My GitHub</a></li>
-				<li><a href="https://s3.amazonaws.com/magicly.work/resume.pdf">My résumé (CV)</a></li>
-			</ul>
-		</Box>
-	</div>
-</main>
+	<Box title="Links">
+		<ul>
+			<li><a href="https://discord.gg/J5hBe8F">C++ Help Discord</a></li>
+			<li><a href="https://github.com/kevinkjt2000">My GitHub <i class="lni lni-github" /></a></li>
+			<li><a href="https://s3.amazonaws.com/magicly.work/resume.pdf">My résumé (CV)</a></li>
+		</ul>
+	</Box>
+</div>
 
 <style>
-	:root {
-		--box-background: white;
-		--box-border: black;
-		--box-shadow: gray;
-	}
-
-	main {
-		display: grid;
-		grid-column-gap: 1rem;
-		grid-auto-rows: min-content;
-		grid-template-columns: 1fr 1fr;
-		grid-template-areas: 'grid-left' 'grid-right';
-		max-width: 1200px;
-	}
-
 	.grid-left {
 		display: grid;
 		grid-row-gap: 1rem;
